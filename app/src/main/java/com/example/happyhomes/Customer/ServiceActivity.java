79772b88d41ac6685d03faef21b14a54a0a84e31
ducteo2003 <103983920+ducteo2003@users.noreply.github.com>
2024-08-10 15:02:59 +0700

    select map

--- app/src/main/java/com/example/happyhomes/Customer/ServiceActivity.java
+++ app/src/main/java/com/example/happyhomes/Customer/ServiceActivity.java
@@ -1,6 +1,8 @@
 package com.example.happyhomes.Customer;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,13 +11,28 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.happyhomes.R;
+import com.example.happyhomes.databinding.ActivityServiceBinding;
 
 public class ServiceActivity extends AppCompatActivity {
 
+    ActivityServiceBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_service);
+        binding = ActivityServiceBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        getdata();
     }
+
+    private void getdata() {
+        Intent intent = getIntent();
+        String address = intent.getStringExtra("address");
+
+        // Hiển thị thông tin vị trí lên giao diện
+        TextView txtLocation = findViewById(R.id.txtLocation);
+        txtLocation.setText("Địa chỉ: " + address);
+    }
+
+
 }
